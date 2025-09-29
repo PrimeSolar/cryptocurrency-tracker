012d3e4f64e58255c1d3a443336819363f609a15
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-09-30 00:23:21 +0300

    Create components.js
    Create an original file of a Library of Custom Reusable Web Components for the Cryptocurrency Project.

---
+++ scripts/components.js
@@ -0,0 +1,126 @@
+// The Library of Custom Reusable Web Components
+// This file is the library containing my collection of custom, reusable web components
+// that can be used across different parts of the project. These components go beyond what browsers provide,
+// allowing for expanded capabilities and functionality in the project.
+
+// The Navigation Bar
+const nav = document.querySelector("nav");
+
+function createNav(nav) {
+  if (nav) {
+    // Define the navigation bar content:
+    nav.innerHTML += `
+      <ul class="nav-menu">
+        <li>
+          <a href="#main" aria-label="View Cryptocurrency Data"
+            >Cryptocurrency Data</a
+          >
+        </li>
+        <li><a href="#about" aria-label="About This Tracker">About</a></li>
+        <li>
+          <a href="#news-section" aria-label="View Cryptocurrency Data"
+            >Cryptocurrency News</a
+          >
+        </li>
+      </ul>
+      `;
+  }
+}
+createNav(nav);
+
+// The Scroll to Top Button
+class ToTop extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML += `
+    <a href="#" aria-label="Scroll to top" title="Scroll to top">
+      <svg width="45px" height="45px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M6 15L12 9L18 15" stroke="#0088ff" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+    </a>
+    `;
+  }
+}
+customElements.define("to-top", ToTop);
+
+const toTop = document
+  .querySelector("body")
+  .appendChild(document.createElement("to-top"));
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
+
+// The Printing Functionality
+
+document.addEventListener("keydown", (event) => {
+  if (event.ctrlKey && event.key === "p") {
+    event.preventDefault();
+    printPage();
+  }
+});
+
+function printPage() {
+  const htmlElement = document.body;
+  if (htmlElement.classList.contains("dark-mode")) {
+    htmlElement.classList.remove("dark-mode");
+    htmlElement.classList.add("light-mode");
+    window.print();
+    htmlElement.classList.remove("light-mode");
+    htmlElement.classList.add("dark-mode");
+  } else {
+    window.print();
+  }
+}
+
+// The Footer
+const footer = document.querySelector("footer");
+const year = new Date().getFullYear();
+function createFooter(footer) {
+  if (footer) {
+    footer.style.display = "flex";
+    footer.style.flexDirection = "column";
+    // Define the footer content:
+    footerContainer.innerHTML += `
+        <ul class="nav-menu">
+          <li>
+            <a href="#main" aria-label="View Cryptocurrency Data"
+              >Cryptocurrency Data</a
+            >
+          </li>
+          <li><a href="#about" aria-label="About This Tracker">About</a></li>
+          <li>
+            <a href="#news-section" aria-label="View Cryptocurrency Data"
+              >Cryptocurrency News</a
+            >
+          </li>
+        </ul>
+        <p id="copyright">Copyright © <a href="https://primesolar.github.io/web-developer/" class="link-primary" rel="noopener noreferrer">Vladislav Kazantsev</a> ${year}</p>
+        <a type="button" href="https://www.buymeacoffee.com/CocaCola" id="bmc-button" target="_blank" rel="noopener noreferrer" aria-label="Buy me a coffee" role="button">☕ Buy me a coffee</a>
+        <a href="https://www.buymeacoffee.com/CocaCola" id="bmc-arrow" target="_blank" rel="noopener noreferrer" aria-label="Buy me a coffee" role="button">⬇</a>
+        <a href="https://www.buymeacoffee.com/CocaCola" id="bmc-link" target="_blank" rel="noopener noreferrer" aria-label="Buy me a coffee" role="button">coff.ee/CocaCola</a>
+      `;
+    function applyFooterStyles() {
+      if (window.matchMedia("(max-width: 768px)").matches) {
+        footer.style.paddingBottom = "4rem";
+      } else {
+        footer.style.paddingBottom = "";
+      }
+    }
+    applyFooterStyles();
+    window.addEventListener("resize", applyFooterStyles);
+  }
+}
+createFooter(footer);
+
+// The Contact Us Link Titles
+const contactUsLinks = document.querySelectorAll("a");
+for (let x of contactUsLinks) {
+  if (x.getAttribute("href") === "contact.html") {
+    x.title = "Navigate to Our Contact Page";
+  }
+}
+
+console.log("components.js is completed");
