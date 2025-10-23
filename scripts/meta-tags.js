f61597be84c74e1422610faa948f79865f587a5f
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-10-23 23:03:22 +0300

    Update meta-tags.js

--- scripts/meta-tags.js
+++ scripts/meta-tags.js
@@ -1,3 +1,33 @@
+/*
+ * Configuration Script for Cryptocurrency Tracker
+ *
+ * This script sets up the essential HTML structure for the Cryptocurrency Tracker web application.
+ * It dynamically inserts metadata, styles, and scripts to configure the document's head and body
+ * elements for optimal SEO, accessibility, and user experience.
+ *
+ * Key Features:
+ * - Sets the default language of the document to English (`lang = "en"`).
+ * - Defines the default text direction as left-to-right (`dir = "ltr"`).
+ * - Configures the document title for the web page.
+ * - Inserts viewport settings to ensure responsive design.
+ * - Includes metadata for author, copyright, description, and keywords to enhance SEO.
+ * - Links to the stylesheet for styling the application.
+ * - Adds a badge to enhance brand identity.
+ * - Loads the Dexie.js library for client-side database management.
+ * - Specifies the type attribute for all script elements for better compatibility.
+ *
+ * Copyright © Vladislav Kazantsev
+ * All rights reserved.
+ * This code is the intellectual property of Vladislav Kazantsev.
+ * You are welcome to clone the related repository and use the code for exploratory purposes.
+ * However, unauthorized reproduction, modification, or redistribution of this code (including cloning of related repository or altering it for activities beyond exploratory use) is strictly prohibited.
+ * Code snippets may be shared only when the original author is explicitly credited and a direct link to the original source of the code is provided alongside the code snippet.
+ * Sharing the link to the file is permitted, except when directed toward retrieval purposes.
+ * Any form of interaction with this file is strictly prohibited when facilitated by the code, except when such interaction is for discussion or exchange purposes with others.
+ * This copyright notice applies globally.
+ * For inquiries about collaboration, usage outside exploratory purposes, or permissions, please contact: hypervisor7@pm.me
+ */
+
 const html = document.querySelector("html");
 const head = document.querySelector("head");
 const body = document.querySelector("body");
